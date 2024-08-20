55c174cb32c7a95dabbde4e4728b1a7abf54b267
mohamednabel-alpha <70145730+mohamednabel-alpha@users.noreply.github.com>
2024-08-20 01:15:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+st.title("First streamlit app")
+st.subheader("Introducing streamlit in automate everything with python")
+st.write("""This is my first web app
+
+enjoy it!
+""")
